007cf4ce0fe6aca27cff8f1b61e2985dfe812f21
Dwayne Robinson <fdwr@hotmail.com>
2026-02-23 16:09:43 -0800

    WrapMlirText add CLI app

---
+++ ProgramCli.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static TextWrapper.TextWrapper;
+
+namespace WrapMlirText
+{
+    internal static class ProgramCli
+    {
+        public static uint DefaultMaximumLineLength = 120;
+        public static uint DefaultLineIndentationPerLevel = 4;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string inputFileName;
+            string outputFileName;
+            uint lineLength = DefaultMaximumLineLength;
+            uint lineIndentation = DefaultLineIndentationPerLevel;
+
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Usage: WrapMlirText <inputTextFile> <outputTextFile> <lineLength> <lineIndent>");
+                return;
+            }
+
+            inputFileName = args[0];
+            outputFileName = args.Length > 1 ? args[1] : "";
+
+            if (args.Length > 2 && (!uint.TryParse(args[2], out lineLength) || lineLength <= 0))
+            {
+                System.Console.WriteLine($"Invalid maximum line length '{args[2]}'.");
+                return;
+            }
+            if (args.Length > 3 && !uint.TryParse(args[3], out lineIndentation))
+            {
+                System.Console.WriteLine($"Invalid line indentation per level '{args[3]}'.");
+            }
+            System.Console.Write($"input filename: {inputFileName}\r\noutput fileName: {outputFileName}\r\nline length: {lineLength}\r\nline indentation: {lineIndentation}\r\n");
+
+            WrapMlirText(inputFileName, outputFileName, lineLength, lineIndentation);
+        }
+
+        static void WrapMlirText(string inputFileName, string outputFileName, uint maximumLineLength, uint lineIndentationPerLevel)
+        {
+            string inputText;
+            string outputText;
+
+            try
+            {
+                inputText = System.IO.File.ReadAllText(inputFileName);
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine($"Error reading input file '{inputFileName}': {ex.Message}");
+                return;
+            }
+
+            var lineBreakpointOpportunities = AssignLineBreakpointOpportunities(inputText, breakPairTable, categoryBreakFlags);
+            var lineRanges = GetLineRanges(inputText, lineBreakpointOpportunities, maximumLineLength, lineIndentationPerLevel);
+            outputText = GetWrappedText(inputText, lineBreakpointOpportunities, lineRanges, lineIndentationPerLevel);
+
+            int oldLineCount = lineBreakpointOpportunities.Count(o => o.MustBreakAfter);
+            int newLineCount = lineRanges.Count;
+            System.Console.Write($"old line count: {oldLineCount}\r\nnew line count: {newLineCount}\r\n");
+
+            if (!string.IsNullOrEmpty(outputFileName))
+            {
+                try
+                {
+                    System.IO.File.WriteAllText(outputFileName, outputText);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine($"Error writing to output file '{outputFileName}': {ex.Message}");
+                    return;
+                }
+            }
+            else // Just print the output text to the console if no output file is specified.
+            {
+                System.Console.Write(outputText);
+            }
+        }
+    }
+}
---
+++ WrapMlirTextCli.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using static TextWrapper.TextWrapper;
+
+namespace WrapMlirText
+{
+    public partial class WrapMlirText
+    {
+        #if false
+        private void buttonWrap_Click(object sender, EventArgs e)
+        {
+            string inputText = textBoxInput.Text;
+            uint maximumLineLength = MaximumLineLength;
+            uint lineIndentationPerLevel = LineIndentationPerLevel;
+
+            var breakpointOpportunities = AssignLineBreakpointOpportunities(inputText, breakPairTable, categoryBreakFlags);
+            var lineRanges = GetLineRanges(inputText, breakpointOpportunities, maximumLineLength, lineIndentationPerLevel);
+
+            string tokensText = GetTokensText(inputText);
+            textBoxTokens.Text = tokensText;
+            textBoxTokens.SelectionStart = 0; // For some reason, setting the text also selects all the text. So clear it.
+            textBoxTokens.SelectionLength = 0;
+            string breakFlagsText = GetBreakFlagsText(inputText, breakpointOpportunities);
+            textBoxBreakFlags.Text = breakFlagsText;
+            textBoxBreakFlags.SelectionStart = 0;
+            textBoxBreakFlags.SelectionLength = 0;
+            string lineRangesText = GetLineRangesText(inputText, breakpointOpportunities, lineRanges, LineIndentationPerLevel);
+            textBoxLineRanges.Text = lineRangesText;
+            textBoxLineRanges.SelectionStart = 0;
+            textBoxLineRanges.SelectionLength = 0;
+            string wrappedText = GetWrappedText(inputText, breakpointOpportunities, lineRanges, LineIndentationPerLevel);
+            textBoxOutput.Text = wrappedText;
+            textBoxOutput.SelectionStart = 0;
+            textBoxOutput.SelectionLength = 0;
+        }
+        #endif
+
+        public static string GetTokensText(string inputText)
+        {
+            var tokensText = new StringBuilder();
+
+            for (uint textPosition = 0; textPosition < inputText.Length;)
+            {
+                uint previousTextPosition = textPosition;
+                TokenCategory category = ReadNextTokenCategory(inputText, ref textPosition);
+                tokensText.Append(category.ToString());
+                tokensText.Append(":\t\"");
+                if (category != TokenCategory.LineBreak)
+                {
+                    tokensText.Append(inputText.Substring((int)previousTextPosition, (int)(textPosition - previousTextPosition)));
+                }
+                tokensText.Append("\"\r\n");
+            }
+            return tokensText.ToString();
+        }
+
+        public static string GetBreakFlagsText(string inputText, TextWrapper.TextWrapper.LineBreakpointOpportunity[] breakpointOpportunities)
+        {
+            var breakFlagsText = new StringBuilder();
+
+            for (uint textPosition = 0; textPosition < inputText.Length; ++textPosition)
+            {
+                breakFlagsText.Append($"[{textPosition}]\t'{inputText[(int)textPosition]}'\tL{breakpointOpportunities[(int)textPosition].indentationLevel}\t{breakpointOpportunities[(int)textPosition].breakFlags}\r\n");
+            }
+            return breakFlagsText.ToString();
+        }
+
+        public static string GetLineRangesText(
+            string inputText,
+            TextWrapper.TextWrapper.LineBreakpointOpportunity[] breakpointOpportunities,
+            List<LineRange> lineRanges,
+            uint lineIndentationPerLevel
+            )
+        {
+            var lineRangesText = new StringBuilder();
+
+            foreach (var lineRange in lineRanges)
+            {
+                string lineText = inputText.Substring((int)lineRange.start, (int)lineRange.Length);
+                if (lineText.EndsWith("\r\n"))
+                {
+                    lineText = lineText.Remove((int)lineText.Length - 2);
+                }
+                var indentationLevel = (lineRange.start < breakpointOpportunities.Length) ? breakpointOpportunities[(int)lineRange.start].indentationLevel : 0;
+                lineRangesText.Append($"@{lineRange.start}..{lineRange.end} x{lineRange.Length} L{indentationLevel}\t\"{lineText}\"\r\n");
+            }
+            return lineRangesText.ToString();
+        }
+    }
+}
