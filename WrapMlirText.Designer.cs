3da1e7bef4e2a32dbeef65979fb674eb6107700c
Dwayne Robinson <fdwr@hotmail.com>
2026-03-09 18:51:00 -0700

    WrapMlirText respond to MouseUp instead of Click in case mouse goes outside control before release

--- WrapMlirText.Designer.cs
+++ WrapMlirText.Designer.cs
@@ -72,8 +72,7 @@
             this.textBoxOutput.TabIndex = 10;
             this.textBoxOutput.Text = "(click the Wrap button)";
             this.textBoxOutput.WordWrap = false;
-            this.textBoxOutput.Click += new System.EventHandler(this.textBoxOutput_SelectionPotentiallyChanged);
-            this.textBoxOutput.DoubleClick += new System.EventHandler(this.textBoxOutput_SelectionPotentiallyChanged);
+            this.textBoxOutput.MouseUp += new System.Windows.Forms.MouseEventHandler(this.textBoxOutput_SelectionPotentiallyChanged);
             this.textBoxOutput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxOutput_SelectionPotentiallyChanged);
             // 
             // textBoxInput
@@ -88,9 +87,8 @@
             this.textBoxInput.Size = new System.Drawing.Size(1000, 168);
             this.textBoxInput.TabIndex = 1;
             this.textBoxInput.Text = resources.GetString("textBoxInput.Text");
-            this.textBoxInput.Click += new System.EventHandler(this.textBoxInput_SelectionPotentiallyChanged);
+            this.textBoxInput.MouseUp += new System.Windows.Forms.MouseEventHandler(this.textBoxInput_SelectionPotentiallyChanged);
             this.textBoxInput.TextChanged += new System.EventHandler(this.textBoxInput_TextChanged);
-            this.textBoxInput.DoubleClick += new System.EventHandler(this.textBoxInput_SelectionPotentiallyChanged);
             this.textBoxInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxInput_SelectionPotentiallyChanged);
             // 
             // labelTokens
@@ -132,8 +130,7 @@
             this.textBoxTokens.TabIndex = 3;
             this.textBoxTokens.Text = "(click the Wrap button)";
             this.textBoxTokens.WordWrap = false;
-            this.textBoxTokens.Click += new System.EventHandler(this.textBoxTokens_SelectionPotentiallyChanged);
-            this.textBoxTokens.DoubleClick += new System.EventHandler(this.textBoxTokens_SelectionPotentiallyChanged);
+            this.textBoxTokens.MouseUp += new System.Windows.Forms.MouseEventHandler(this.textBoxTokens_SelectionPotentiallyChanged);
             this.textBoxTokens.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxTokens_SelectionPotentiallyChanged);
             // 
             // labelInputText
@@ -194,8 +191,7 @@
             this.textBoxBreakFlags.TabIndex = 5;
             this.textBoxBreakFlags.Text = "(click the Wrap button)";
             this.textBoxBreakFlags.WordWrap = false;
-            this.textBoxBreakFlags.Click += new System.EventHandler(this.textBoxBreakFlags_SelectionPotentiallyChanged);
-            this.textBoxBreakFlags.DoubleClick += new System.EventHandler(this.textBoxBreakFlags_SelectionPotentiallyChanged);
+            this.textBoxBreakFlags.MouseUp += new System.Windows.Forms.MouseEventHandler(this.textBoxBreakFlags_SelectionPotentiallyChanged);
             this.textBoxBreakFlags.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxBreakFlags_SelectionPotentiallyChanged);
             // 
             // labelLineRanges
@@ -219,8 +215,7 @@
             this.textBoxLineRanges.TabIndex = 7;
             this.textBoxLineRanges.Text = "(click the Wrap button)";
             this.textBoxLineRanges.WordWrap = false;
-            this.textBoxLineRanges.Click += new System.EventHandler(this.textBoxLineRanges_SelectionPotentiallyChanged);
-            this.textBoxLineRanges.DoubleClick += new System.EventHandler(this.textBoxLineRanges_SelectionPotentiallyChanged);
+            this.textBoxLineRanges.MouseUp += new System.Windows.Forms.MouseEventHandler(this.textBoxLineRanges_SelectionPotentiallyChanged);
             this.textBoxLineRanges.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBoxLineRanges_SelectionPotentiallyChanged);
             // 
             // labelWrapWidth
